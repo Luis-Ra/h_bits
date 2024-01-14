e0ce0dcbf8aa6a9aae2d919690873cb6cb9a20a8
Luis-Ra <luis.r.2000.l@gmail.com>
2024-01-14 18:33:36 -0300

    finalizacion

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:h_bits/components/my_frawer.dart';
 import 'package:h_bits/components/my_habit_tile.dart';
+import 'package:h_bits/components/my_heat_map.dart';
 import 'package:h_bits/database/habit_database.dart';
 import 'package:h_bits/models/habit.dart';
 import 'package:provider/provider.dart';
@@ -77,14 +78,92 @@ class _HomeScreenState extends State<HomeScreen> {
   }
 
   // edit habit  box
+  void editHabitBox(Habit habit) {
+    // set the controller's text to the habit's current name
+    textController.text = habit.name;
+
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        content: TextField(
+          controller: textController,
+        ),
+        actions: [
+          // save button
+          MaterialButton(
+            onPressed: () {
+              // get the new habit name
+              String newHabitName = textController.text;
+
+              // save to DB
+              context
+                  .read<HabitDatabase>()
+                  .updateHabitNmae(habit.id, newHabitName);
+
+              // pop box
+              Navigator.pop(context);
+
+              // clear controller
+              textController.clear();
+            },
+            child: const Text('Save'),
+          ),
+          // cancel button
+          MaterialButton(
+            onPressed: () {
+              // pop box
+              Navigator.pop(context);
+
+              // clear the controller
+              textController.clear();
+            },
+            child: const Text('Cancel'),
+          )
+        ],
+      ),
+    );
+  }
 
   // delete habit box
+  void deleteHabitBox(Habit habit) {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: const Text('Are you sure you want to delete?'),
+        actions: [
+          // delete button
+          MaterialButton(
+            onPressed: () {
+              // save to DB
+              context.read<HabitDatabase>().deleteHabit(habit.id);
+
+              // pop box
+              Navigator.pop(context);
+            },
+            child: const Text('Delete'),
+          ),
+          // cancel button
+          MaterialButton(
+            onPressed: () {
+              // pop box
+              Navigator.pop(context);
+            },
+            child: const Text('Cancel'),
+          )
+        ],
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Theme.of(context).colorScheme.background,
-      appBar: AppBar(),
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+        foregroundColor: Theme.of(context).colorScheme.inversePrimary,
+      ),
       floatingActionButton: FloatingActionButton(
         onPressed: createNewHabit,
         elevation: 0,
@@ -94,7 +173,42 @@ class _HomeScreenState extends State<HomeScreen> {
         ),
       ),
       drawer: const MyDrawer(),
-      body: _buildHabitList(),
+      body: ListView(
+        children: [
+          // heat map
+          _buildHeatMap(),
+
+          // habit list
+          _buildHabitList(),
+        ],
+      ),
+    );
+  }
+
+  // build heat map
+  Widget _buildHeatMap() {
+    // habit database
+    final habitDatabase = context.watch<HabitDatabase>();
+
+    // current habits
+    List<Habit> currentHabits = habitDatabase.curretHabits;
+
+    // return heatmap UI
+    return FutureBuilder(
+      future: habitDatabase.getFirstLaunchDate(),
+      builder: (context, snapshot) {
+        // once the data is available -> build heatmap
+        if (snapshot.hasData) {
+          return MyHeatMap(
+              startDate: snapshot.data!,
+              datasets: prepHeatMapDataset(currentHabits));
+        }
+
+        // handle case whre no data is returned
+        else {
+          return Container();
+        }
+      },
     );
   }
 
@@ -109,6 +223,8 @@ class _HomeScreenState extends State<HomeScreen> {
     // return the list to the UI
     return ListView.builder(
       itemCount: currentHabits.length,
+      shrinkWrap: true,
+      physics: const NeverScrollableScrollPhysics(),
       itemBuilder: (BuildContext context, int index) {
         // get ech individual habit
         final habit = currentHabits[index];
@@ -120,6 +236,8 @@ class _HomeScreenState extends State<HomeScreen> {
           isCompleted: isCompledToday,
           text: habit.name,
           onChanged: (value) => checkHabitOnOff(value, habit),
+          editHabit: (context) => editHabitBox(habit),
+          deleteHabit: (context) => deleteHabitBox(habit),
         );
       },
     );
